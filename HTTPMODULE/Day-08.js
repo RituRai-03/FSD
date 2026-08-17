04519397938ad9d2a4c2511f89897d522e1c71d5
2025b01010374 <ritu.25b01010374@abes.ac.in>
2026-08-17 10:36:41 +0530

    FSD Day-08

---
+++ HTTPMODULE/Day-08.js
@@ -0,0 +1,12 @@
+//Create your own server using http module
+
+import http from "http";
+const server=http.createServer((req,res)=>{
+    res.writeHead(200,{"Content-Type":"text/html"});
+    res.write("<h1>Welcome to my server</h1>");  
+    // res.write("HEllo World");
+    res.end();
+})
+server.listen(3000,()=>{
+ console.log("Server is running on port 3000");    
+})
