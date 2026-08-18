22d646a9c935eb232553f05cc385e8bd05cf7314
2025b01010374 <ritu.25b01010374@abes.ac.in>
2026-08-18 16:11:22 +0530

    FSD Day-08

--- HTTPMODULE/Day-08.js
+++ HTTPMODULE/Day-08.js
@@ -3,10 +3,11 @@
 import http from "http";
 const server=http.createServer((req,res)=>{
     res.writeHead(200,{"Content-Type":"text/html"});
+    res.write("<h1>Name: Ritu Rai</h1>")
     res.write("<h1>Welcome to my server</h1>");  
     // res.write("HEllo World");
     res.end();
 })
-server.listen(3000,()=>{
- console.log("Server is running on port 3000");    
+server.listen(3001,()=>{
+ console.log("Server is running on port 3001");    
 })
