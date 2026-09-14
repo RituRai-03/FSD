6869ed287a338b5fda043093a57bcdf4bbc00e32
2025b01010374 <ritu.25b01010374@abes.ac.in>
2026-09-14 09:40:28 +0530

    Express

--- Express_HTML/Server.js
+++ Express_HTML/Server.js
@@ -1,18 +1,46 @@
 import express from 'express';
 import fs from 'fs';
-const app=express();
-const PORT=3000;
-app.get('/',(req,res)=>{
-fs.readFile('./pages/Index.html','utf-8', (err,data)=>
-{
-if(err){
-    res.status(500).send("Error reading file");
-    return;
-}
-res.send(data);
-});
+
+const app = express();
+const PORT = 3000;
+
+// Home
+app.get('/', (req, res) => {
+    fs.readFile('./pages/Index.html', 'utf-8', (err, data) => {
+        if (err) {
+            res.status(500).send("Error reading Index.html");
+            return;
+        }
+
+        res.send(data);
+    });
 });
 
-app.get('/About', (req,res)=>{})
-app.get('/Contact', (req,res)=>{})
-app.listen(PORT, () => {})
+// About
+app.get('/About', (req, res) => {
+    fs.readFile('./pages/About.html', 'utf-8', (err, data) => {
+        if (err) {
+            res.status(500).send("Error reading About.html");
+            return;
+        }
+
+        res.send(data);
+    });
+});
+
+// Contact
+app.get('/Contact', (req, res) => {
+    fs.readFile('./pages/contact.html', 'utf-8', (err, data) => {
+        if (err) {
+            res.status(500).send("Error reading contact.html");
+            return;
+        }
+
+        res.send(data);
+    });
+});
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
